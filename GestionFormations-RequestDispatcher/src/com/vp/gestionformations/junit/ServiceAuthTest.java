bc33e93fa88b732fe35085332f9408b368264621
Vincent <panouilleres.vincent@yahoo.fr>
2016-08-25 17:05:05 +0200

    Minor fix

--- GestionFormations-RequestDispatcher/src/com/vp/gestionformations/junit/ServiceAuthTest.java
+++ GestionFormations-RequestDispatcher/src/com/vp/gestionformations/junit/ServiceAuthTest.java
@@ -1,7 +1,5 @@
 package com.vp.gestionformations.junit;
 
-import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Test;
 
--- GestionFormations-RequestDispatcher/src/com/vp/gestionformations/presentation/MaServlet2.java
+++ GestionFormations-RequestDispatcher/src/com/vp/gestionformations/presentation/MaServlet2.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.vp.gestionformations.dao.UserDAO;
 import com.vp.gestionformations.domaine.User;
 import com.vp.gestionformations.service.UserService;
 
