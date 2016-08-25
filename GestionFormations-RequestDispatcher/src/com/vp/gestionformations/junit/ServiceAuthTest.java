34a259a11b2a9362e758d89f9ba6bc997662ad75
Vincent <panouilleres.vincent@yahoo.fr>
2016-08-25 16:52:31 +0200

    Ajout Junit

---
+++ GestionFormations-RequestDispatcher/src/com/vp/gestionformations/junit/ServiceAuthTest.java
@@ -0,0 +1,35 @@
+package com.vp.gestionformations.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vp.gestionformations.service.ServiceAuth;
+
+public class ServiceAuthTest {
+	
+	@Test
+	public void testEstValide1() {
+		String login ="manga";
+		String pwd ="aimejava";
+		boolean status;
+		
+		ServiceAuth servicePourTest = new ServiceAuth();
+		status = servicePourTest.estValide(login, pwd);
+		
+		Assert.assertTrue(status);		
+	}
+	
+	@Test
+	public void testEstValide2() {
+		String login ="ddddd";
+		String pwd ="eeee";
+		boolean status;
+		
+		ServiceAuth servicePourTest = new ServiceAuth();
+		status = servicePourTest.estValide(login, pwd);
+		
+		Assert.assertFalse(status);		
+	}
+}
