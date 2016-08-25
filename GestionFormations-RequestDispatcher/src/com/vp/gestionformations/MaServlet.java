8149a65f394b6655ca71a835731d7d08f18dad30
Vincent <panouilleres.vincent@yahoo.fr>
2016-08-25 11:58:49 +0200

    Partie 3 du tp

--- GestionFormations-RequestDispatcher/src/com/vp/gestionformations/MaServlet.java
+++ GestionFormations-RequestDispatcher/src/com/vp/gestionformations/MaServlet.java
@@ -16,47 +16,50 @@ import javax.servlet.http.HttpServletResponse;
 public class MaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public MaServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public MaServlet() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		traitement(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//Etape 1: Recuperation des parametres de la requete
-		
+		traitement(request, response);
+	}
+
+	protected void traitement(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// Etape 1: Recuperation des parametres de la requete
+
 		String login = request.getParameter("loginuser");
-		String pwd   = request.getParameter("mdpuser");
-				
+		String pwd = request.getParameter("mdpuser");
+
 		// Etape 2: Soumettre les parametres de la requete à la couche service
-				
-				
+
 		// Etape 3: Repond à l'utilisateur
-		
+
 		RequestDispatcher dispatcher;
-		
-		if(("manga".equalsIgnoreCase(login))&&("aimejava".equalsIgnoreCase(pwd))){
+
+		if (("manga".equalsIgnoreCase(login)) && ("aimejava".equalsIgnoreCase(pwd))) {
 			dispatcher = request.getRequestDispatcher("Acceuil.html");
-		}else{
+		} else {
 			dispatcher = request.getRequestDispatcher("login.html");
 		}
-		
-		dispatcher.forward(request, response);
-		
 
-		//doGet(request, response);
+		dispatcher.forward(request, response);
+
 	}
 
 }
