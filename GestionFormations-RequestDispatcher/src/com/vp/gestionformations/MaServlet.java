5dd674d358f874c47d9c3c5e26587bd839f33e37
Vincent <panouilleres.vincent@yahoo.fr>
2016-08-25 11:46:13 +0200

    Partie 1 et 2 Tp 8

---
+++ GestionFormations-RequestDispatcher/src/com/vp/gestionformations/MaServlet.java
@@ -0,0 +1,62 @@
+package com.vp.gestionformations;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MaServlet
+ */
+@WebServlet("/MaServlet")
+public class MaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MaServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Etape 1: Recuperation des parametres de la requete
+		
+		String login = request.getParameter("loginuser");
+		String pwd   = request.getParameter("mdpuser");
+				
+		// Etape 2: Soumettre les parametres de la requete à la couche service
+				
+				
+		// Etape 3: Repond à l'utilisateur
+		
+		RequestDispatcher dispatcher;
+		
+		if(("manga".equalsIgnoreCase(login))&&("aimejava".equalsIgnoreCase(pwd))){
+			dispatcher = request.getRequestDispatcher("Acceuil.html");
+		}else{
+			dispatcher = request.getRequestDispatcher("login.html");
+		}
+		
+		dispatcher.forward(request, response);
+		
+
+		doGet(request, response);
+	}
+
+}
